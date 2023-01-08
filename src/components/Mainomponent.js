9619a4d8f60a4782da6668d7a1f4b23b66aff969
Ankit-Sharma-8110 <80452049+Ankit-Sharma-8110@users.noreply.github.com>
2023-01-08 17:26:00 +0530

    single page applications part one

--- src/components/Mainomponent.js
+++ src/components/Mainomponent.js
@@ -1,18 +1,25 @@
 import React, { Component } from 'react';
 import Home from './HomeComponent';
 import Menu from './MenuComponent';
-import { DISHES } from '../shared/dishes'
+import Contact from './ContactComponent';
+import { DISHES } from '../shared/dishes';
+import { COMMENTS } from '../shared/comments';
+import { PROMOTIONS } from '../shared/promotions';
+import { LEADERS } from '../shared/leaders';
 import DishDetail from "./DishdetailComponent";
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import { Switch, Route, Redirect } from 'react-router-dom';
 
 class Main extends Component {
-  constructor (props) {
+  constructor(props) {
     super(props);
 
     this.state = {
       dishes: DISHES,
+      comments: COMMENTS,
+      promotions: PROMOTIONS,
+      leaders: LEADERS,
       // selectedDish: null
     }
   }
@@ -21,27 +28,32 @@ class Main extends Component {
   //     this.setState({ selectedDish: dishId })
   // }
 
-  render () {
+  render() {
 
     const HomePage = () => {
-      return(
-        <Home />
+      return (
+        <Home
+          dish={this.state.dishes.filter((dish) => dish.featured)[0]}
+          promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
+          leader={this.state.leaders.filter((leader) => leader.featured)[0]}
+        />
       );
     }
     return (
       <div>
         <Header />
         <Switch>
-        <Route path="/home" component={HomePage} />
-        <Route exact path="/menu" component={() => <Menu dishes={this.state.dishes} />} />
-        <Redirect to="/home" />
+          <Route path="/home" component={HomePage} />
+          <Route exact path="/menu" component={() => <Menu dishes={this.state.dishes} />} />
+          <Route exact path="/contactus" component={Contact} />
+          <Redirect to="/home" />
         </Switch>
         {/* <Menu dishes={this.state.dishes} onClick={(dishId) => this.onDishSelect(dishId)} /> */}
         {/* <DishDetail dish={this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0] } /> */}
         <Footer />
       </div>
     );
-    }
+  }
 }
 
 export default Main;
